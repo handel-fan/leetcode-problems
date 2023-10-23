f72f49fe522a85a88ca2b32459a5e1f2a7f839e8
Jegan Sivakasiulaganathan <jegan.sivakasiulaganathan@kbra.com>
2023-10-23 11:01:58 -0400

    fixed tests

--- swap_pairs/test_swap_pairs.py
+++ swap_pairs/test_swap_pairs.py
@@ -6,22 +6,13 @@ class SwapPairsTests:
 
     def base_test(self, head:swap_pairs.ListNode, expected_head:swap_pairs.ListNode):
 
-        if head is None and expected_head is None:
-            return True
-
-        while head.next is not None and expected_head.next is not None:
-            if head is None or expected_head is None:
-                return False
+        while head is not None and expected_head is not None:
             if head.val != expected_head.val:
                 return False
             head = head.next
             expected_head = expected_head.next
 
         if head is None or expected_head is None:
-            return False
-        if head.next is None or expected_head.next is None:
-            return False
-        if head.val != expected_head.val:
             return False
         return True
 
